f794dddd9cdae36b7b7e0a844ee24197bd891a72
Bharat Shah <akashche72@gmail.com>
2024-05-15 20:08:35 +0400

    light design is done

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { squareStyling } from './utils/squareStyling';
 import { EDifficultLevel } from './interfaces/chess.interface';
 import { ETeam } from './interfaces/chess.interface';
 
+
 import './styles/main.scss';
 import './app.scss';
 
@@ -162,17 +163,17 @@ const App: React.FC = () => {
   }
 
   const undoGame = () => {
-  chess.undo();
-  // Undo the second last move (user's move)
-  chess.undo();
+    chess.undo();
+    // Undo the second last move (user's move)
+    chess.undo();
 
-  // Update the FEN string to the new position
-  setFen(chess.fen());
-  // Update the move history
-  setHistory(chess.history({ verbose: true }));
+    // Update the FEN string to the new position
+    setFen(chess.fen());
+    // Update the move history
+    setHistory(chess.history({ verbose: true }));
 
-  // Remove the last two moves from the game history
-  setGameHistory(prevHistory => prevHistory.slice(0, -2));
+    // Remove the last two moves from the game history
+    setGameHistory(prevHistory => prevHistory.slice(0, -2));
   }
 
   // Without even changing was re-rendering 4 times
@@ -181,11 +182,92 @@ const App: React.FC = () => {
 
   return (
     <div className={`app ${darkModeMemo ? 'dark-mode' : ''}`}>
+      <div className="wrapper">
+        <div className="col right">
+          <div className="row computer">
+            <img src='/svg/computer.svg' width={20} height={20} alt='' />
+            <div>Computer</div>
+          </div>
+          <div className="row chessboard">
+            <Chessboard
+              width={400}
+              position={fen}
+              // onDrop prop tracks every time a piece is moved.
+              // The rest is handled in the the handleMove function.
+              onDrop={onDrop}
+              boardStyle={{
+                borderRadius: '5px',
+                boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
+              }}
+              squareStyles={squareStyles}
+              dropSquareStyle={dropSquareStyle}
+              onDragOverSquare={onDragOverSquare}
+              onSquareClick={onSquareClick}
+              onMouseOutSquare={onMouseOutSquare}
+              onMouseOverSquare={(square) => onMouseOverSquare(chess, square, highlightSquare)}
+              darkSquareStyle={{ backgroundColor: '#f0f0f0' }}
+              lightSquareStyle={{ backgroundColor: 'gray' }}
+              allowDrag={() => false}
+              draggable={false}
+            />
+          </div>
+          <div className="row guest">
+            <img src='/svg/guest.svg' width={20} height={20} alt='' />
+            <div>Guest</div>
+          </div>
+        </div>
+        <div className="col">
 
-      <Header
+          <Button type='square' variant='primary'>React Chessboard</Button>
+          <div className="histories">
+            <div className="items">
+              <div className="item">1</div>
+              <div className="item">er</div>
+              <div className="item">45</div>
+            </div>
+
+            <div className="items">
+              <div className="item">1</div>
+              <div className="item">er</div>
+              <div className="item">45</div>
+            </div>
+          </div>
+
+          <div className="action-wrapper">
+            <div className="actions">
+              <div className="title">Actions</div>
+              <div className="actions-buttons">
+                <Button small type='square' variant='dark' onClick={() => undoGame()}>Rematch</Button>
+                <Button small type='square' variant='dark' onClick={() => undoGame()}>Undo</Button>
+                <Button small type='square' variant='red' onClick={() => undoGame()}>Resign</Button>
+              </div>
+            </div>
+
+            <div className="actions">
+              <div className="title">Difficulty</div>
+              <div className="actions-buttons">
+                <Button small type='square' variant='yellow' onClick={() => undoGame()}>Easy</Button>
+                <Button small type='square' variant='dark__2' onClick={() => undoGame()}>Medium</Button>
+                <Button small type='square' variant='dark__2' onClick={() => undoGame()}>Hard</Button>
+              </div>
+            </div>
+
+            <div className="actions">
+              <div className="title">Theme</div>
+              <div className="actions-buttons theme">
+                <Button type='square' variant='dark__3'>Dark Mode</Button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+      {/* <Header
         darkModeOnChangeHandler={darkModeOnChangeHandler}
-      />
-      <div className='container'>
+      /> */}
+      {/* <div className='container'>
         {!startGame && <Welcome setStartGame={setStartGame} />}
 
         {startGame && <section className='game'>
@@ -223,7 +305,7 @@ const App: React.FC = () => {
             <Table data={gameHistory} />
           </div>
         </section>}
-      </div>
+      </div> */}
     </div>
 
   );
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,22 +1,47 @@
 import React, { ReactNode } from 'react'; 
 import styles from './button.module.scss';
+import './button.scss';
 
 type buttonType = 'round' | 'square';
-type buttonVariant = 'primary' | 'secondary';
+type buttonVariant = 'primary' | 'secondary' | 'dark' | 'red' | 'yellow' | 'dark__2' | 'dark__3';
 
 interface ButtonInterface {
     children:ReactNode;
     type: buttonType;
     variant: buttonVariant;
+    small?: boolean;
     [x:string]:any;
 }
-export default function Button({children, type, variant, ...rest}: ButtonInterface) {
+export default function Button({children, type, variant, small, ...rest}: ButtonInterface) {
   
-  let buttonBaseClass = styles.btn;
+  let buttonBaseClass = small ? styles.btn__small : styles.btn;
   
   if(variant === 'primary') {
-    buttonBaseClass += ' ' + styles.primary;
+    buttonBaseClass += ' ' + styles.primary; 
   }
+  if(variant === 'dark') {
+    buttonBaseClass += ' ' + styles.dark;
+  }
+
+  if(variant === 'red') {
+    buttonBaseClass += ' ' + styles.red;
+  }
+
+  if(variant === 'yellow') {
+    buttonBaseClass += ' ' + styles.yellow;
+  }
+
+
+  if(variant === 'dark__2') {
+    buttonBaseClass += ' ' + styles.dark__2;
+  
+  }
+
+  if(variant === 'dark__3') {
+    buttonBaseClass += ' ' + styles.dark__3;
+  
+  }
+  
 
   if(type === 'round') {
     buttonBaseClass += ' ' + styles.round;
